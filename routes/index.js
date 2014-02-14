5213218f1851176f0e73eabe41306c7915a39431
Alejandro Rodriguez <arodrig1@stanford.edu>
2014-02-14 07:31:05 +0000

    Added help and about page placeholders.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-
-/*
- * GET home page.
- */
-
 exports.view = function(req, res){
   // @TODO: Check for logged in user. If logged in, redirect to appropriate home page.
   res.render('index');
+};
+
+exports.about = function(req, res){
+  res.render('about');
+};
+
+exports.help = function(req, res){
+  res.render('help');
 };
