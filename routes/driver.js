ebcc5dd2e94f769de36e5dbf89dd2443e4c41cae
Divya Garg <divyag@stanford.edu>
2014-02-13 23:23:37 -0800

    added basic json, need to actually fill it with data and also render it

--- routes/driver.js
+++ routes/driver.js
@@ -1,11 +1,14 @@
+var data = require("../data.json");
+
 exports.home = function(req, res){
 	console.log("Loading driver's rides from database...");
+	//data["riders"]
 	var rides = [];
-  	res.render('driver/home');
+  	res.render('driver/home', data);
 };
 
 exports.calendar = function(req, res){
 	console.log("Loading driver's calendar from database...");
 	var rides = [];
-  	res.render('driver/calendar');
+  	res.render('driver/calendar', data);
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-
+var data = require("../data.json");
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
   // @TODO: Check for logged in user. If logged in, redirect to appropriate home page.
-  res.render('index');
+  res.render('index', data);
 };
