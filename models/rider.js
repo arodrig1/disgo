b022145b0ec1335c3aaa1bdc443698d4bec94f57
Divya Garg <divyag@stanford.edu>
2014-02-20 01:19:54 -0800

    UNFINISHED rider examples for querying

--- models/rider.js
+++ models/rider.js
@@ -1,19 +1,35 @@
 var Rider = function() {
     var mongoose = require('mongoose'),
         Schema = mongoose.Schema,
-        _ObjectId = mongoose.Types.ObjectId;
+        _ObjectId = mongoose.Types.ObjectId,
+        Ride = require('./ride.js');
     
     var RiderSchema = new Schema({
-        id: { type: Number, required: true },
         name: { type: String, required: true },
-        username: { type: String, required: true }
+        username: { type: String, required: true },
+        rides: [{ type: ObjectId, ref: 'Ride' }]
     });
+
+    var _findById = function (riderId, callback) {
+        _model.findById(new ObjId(riderId)).populate('name username rides').exec(callback);
+    }
+
+    var _findAll = function(callback) {
+        _model.find().populate('name username rides').exec(callback);
+    }
+
+    var _save = function (riderJSON, callback) {
+        _model.create(riderJSON, callback);
+    }
 
     var _model = mongoose.model('Rider', RiderSchema);
 
-
     return {
-        schema: RiderSchema
+        schema: RiderSchema,
+        model: _model,
+        findById: _findById,
+        findAll: _findAll,
+        save: _save
     };
 }();
 
--- routes/rider.js
+++ routes/rider.js
@@ -1,7 +1,30 @@
-var data = require("../data.json");
+var mongoose = require('mongoose'),
+Coordinator = require('../models/coordinator.js'),
+Driver = require('../models/driver.js'),
+Rider = require('../models/rider.js'),
+Ride = require('../models/ride.js'),
+User = require('../models/user.js'),
+ObjectId = mongoose.Types.ObjectId;
 
-exports.home = function(req, res){
+var _home = function(req, res){
 	console.log("Loading user's rides from database...");
+
 	var rides = [];
   	res.render('rider/home', data);
-};
+}
+
+var _findById = function(req, res) {
+    Rider.findById(req.params.id, function(err, riderDocs) {
+        if (err) {
+            throw err;
+        }
+        res.set('cache-control', 'no-cache');
+        res.set('Content-Type', 'application/json');
+        res.send(riderDocs);
+    });
+}
+
+module.exports = {
+    home: _home,
+    findById: _findById
+}
