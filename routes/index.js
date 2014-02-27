113f44386a3ebb634f64b38f43289d7d22bf273d
vagrant <vagrant@precise32.(none)>
2014-02-27 02:20:42 +0000

    made help page customizable to rider/driver and fixed about page

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,14 @@ var _about = function(req, res){
 }
 
 var _help = function(req, res){
-	res.render('help');
+  var message = "";
+  if(req.user.type == 0){
+    message = "<h4> Viewing Your Schedule </h4> <div class = \"container\"><p>Your live schedule should be displayed on your home screen.  Make sure to refresh to see if new rides have been added! If you still don't see any rides, you may not have any scheduled.</p></div><h4> Other Issues?</h4> <div class = \"container\"><p>Contact our support team directly by emailing us at disgoapp.support@gmail.com. <br> We'll try to get back to you as soon as possible. </p></div><br><br><br>";
+  }else{
+    message = "<h4>Submitting a Ride Request</h4><p class = \"container\">To submit a ride, click the large blue button labeled \"Request a Ride\" and fill out the form. Our carts are not permited to drive everywhere on campus, so if you can't find your location in the drop down menu, choose the closest point!</p><h4>Editing a Ride Request</h4><p class = \"container\">To edit a ride request, click the blue edit button at the bottom of each ride and fill out the appropriate form. </p><h4>Viewing the Schedule</h4><p class = \"container\">Your home screen should also serve as a comprehensive schedule.  If you don't see any rides you may not have any scheduled. </p><h4>Other Issues?</h4><p class = \"container\">Contact our support team directly by emailing us at disgoapp.support@gmail.com. <br> We'll try to get back to you as soon as possible. </p><br><br><br>";
+  }
+	res.render('help', { 'message' : message});
+  //req.user.type; //0 is driver 1 is rider
 }
 
 module.exports = {
